e6252cbe523710a7cc6e42174976f67d4905dc8b
ispartners.cathy@gmail.com <ispartners.cathy@gmail.com@5935c2f7-85d4-7600-3b0a-1c2845b76233>
2012-11-23 08:27:56 +0000

    [sua] download.js

---
+++ trunk/common/js/chk_device.js
@@ -0,0 +1,7 @@
+//<![CDATA[
+if (document.referrer.indexOf('iijima-dental.com') == -1 && ((navigator.userAgent.indexOf('iPhone') > 0 && navigator.userAgent.indexOf('iPad') == -1) || navigator.userAgent.indexOf('iPod') > 0 || (navigator.userAgent.indexOf('Android') > 0 && navigator.userAgent.indexOf('Mobile') > 0))) {
+    if(confirm('Chuyen doi qua giao dien smartphone?')) {
+        location.href = 'http://iijima-dental.smart-change.info/';
+    }
+}
+//]]>
