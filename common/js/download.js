c094c55085879358ebc43da265dff68ede249e0a
ispartners.cathy <ispartners.cathy@gmail.com>
2012-11-23 08:25:27 +0000

    [sua] download.js

--- common/js/download.js
+++ common/js/download.js
@@ -12,20 +12,9 @@ else
 }
 
 var sPageN = sPage.replace('.html','');
-/* doan js ban dau, neu khong duoc thi tra lai tai day
-$(function(){
-$('#content').append('<dl class="download">\
-						 <dt>Download</dt>\
-						 <dd>\
-							 <ul>\
-							 	<li>JS :<a href="common/js/jquery-1.3.2.min.js">jquery-1.3.2.min.js</a>and<a href="common/js/'+sPageN+'.js">'+sPageN+'.js</a></li>\
-								<li>CSS :<a href="css/'+sPageN+'.css">'+sPageN+'.css</a></li><li>HTML :<a href="'+sPageN+'.html">'+sPageN+'.html</a></li>\
-								</ul>\
-						  </dd>\
-						</dl>');
-});*/
-
-// doan js nay dang thu
+/*
+//--start
+doan js ban dau, neu khong duoc thi tra lai tai day
 $(function(){
 $('#content').append('<dl class="download">\
 						 <dt>Download</dt>\
@@ -37,30 +26,61 @@ $('#content').append('<dl class="download">\
 						  </dd>\
 						</dl>');
 });
+//--end
+*/
 
-if(ajaxFun('common/js/'+sPageN+'.js')!=null){
-		alert('common/js/'+sPageN+'.js');
-		
+// doan js dung de append
+$(function(){
+$('#content').append('<dl class="download"><dt>Download</dt><dd><ul>');
+
+$('dl.download ul').append('<li>JS :<a href="common/js/jquery-1.3.2.min.js">jquery-1.3.2.min.js</a>-- \
+						   <a href="common/js/'+sPageN+'.js">'+sPageN+'.js</a></li>',exists('css/'+sPageN+'.css'),
+																																									   '<li>HTML :<a href="'+sPageN+'.html">'+sPageN+'.html</a></li>\
+								</ul>\
+						  </dd>\
+						</dl>');
+
+});
+
+
+
+function exists(nCss){
+var t = ajaxFun(nCss);
+	if(t==null) {
+		return '<li>CSS :<a href="css/'+sPageN+'.css">'+sPageN+'.css</a></li>';
 	}
-	else {alert('y');}
-	
+	else{
+		return false;
+		}
+}
+
+
 function ajaxFun(nSrc){
 	var kq;
-	
+//--start 
+//doan js nay dung de lay phan duong dan (khong lay ten trang web)
+var sPath = window.location.pathname;
+//var pageName = sPath.substring(sPath.lastIndexOf('\\') + 1);
+var pageName = sPath.substring(sPath.lastIndexOf('/') + 1);
+var href= location.href;
+var nSrc1 = href.replace(pageName,nSrc);
+//--end
+
 	$.ajax({
-			url: nSrc,
+			url: nSrc1,
+			dataType: "js", // data dung de xac dinh du lieu can kiem tra
+			dataType: "css", // neu la duong dan binh thuong thi khong can de data
 			//url:'http://wwww.google.com',
 			type:'HEAD',
 			error:
 				function(){
-					alert(nSrc+' not EXISTS');
-					kq=nSrc;
+				 kq=nSrc;
 				},
 			success:
 				function(){
-					alert(nSrc+' EXISTS');
-					   return true;  
+					return true;
 				}
 		});
-	return kq; /*not Exists*/
+	
+	return kq;
 }
