56cd0363e1d9e9f947f0e60b40f7a5db16f3c2a5
ispartners.cathy <ispartners.cathy@gmail.com>
2014-01-16 08:38:50 +0000

    [code editor]

---
+++ htn_webuda_com/js/codeeditor.js
@@ -0,0 +1,64 @@
+$(function(){
+	
+	$('.str_syntax').after('<div class="temp" style="display:none"></div>');
+	h = $('.temp').height();
+	$('.temp').html($('.CodeMirror-code').html());
+	$('.temp > div > div').remove();
+	var codeHtml = $('.temp').text();
+	$('.out').attr('srcdoc',codeHtml);
+	
+	
+$('.btnChange').click(function(){
+	/*$('.str_syntax').after('<div class="temp" style="display:none"></div>');*/
+	h = $('.temp').height();
+	$('.temp').html($('.CodeMirror-code').html());
+	$('.temp > div > div').remove();
+	var codeHtml = $('.temp').text();
+	//var divContent = $t.next('.CodeMirror-code').text();
+	$('.out').attr('srcdoc',codeHtml);
+	/*calcHeight();*/
+	ResizeIframe('preview');
+});
+
+
+
+
+
+
+
+
+
+$(window).load(function(){
+	
+	var t = setTimeout(function(){
+			$('#preview').css('display','block');
+			var frame = document.getElementById('preview');
+			frame.height = frame.contentWindow.document.body.scrollHeight-20 + "px";
+			$('#preview').css('width',frame.contentWindow.document.body.scrollWidth + "px");	
+	
+	},200);
+});
+
+
+
+function ResizeIframe(id){
+	var t = setTimeout(function(){
+	$('#preview').css('display','block');
+	$('#'+id).css({'width':'auto','height':'auto'});
+	var frame = document.getElementById(id);
+	frame.height = frame.contentWindow.document.body.scrollHeight + "px";
+	$('#'+id).css('width',frame.contentWindow.document.body.scrollWidth + "px");
+	},200);
+}
+
+
+
+
+
+
+
+
+
+
+});
+
